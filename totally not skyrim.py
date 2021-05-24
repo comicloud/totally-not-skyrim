5718fabe41ad5256049c3555f3b6ff822376f2f1
comicloud <80374048+comicloud@users.noreply.github.com>
2021-05-24 12:44:12 -0400

    Add files via upload

---
+++ totally not skyrim.py
@@ -0,0 +1,23 @@
+def game():
+
+    answer=input('Hey, you. Youre finally awake')
+    
+    if answer.lower()== 'Huh':
+        print ('Ralof; you were trying to cross the border right? walked right into that Imperal ambush, same as us, and that thief over there.')
+        start=True
+        inventory=[]
+        
+    else:
+        print('Ralof; oh, guess ye arnt the dragonborn')
+        
+    if start==True:
+        print('Lokir: D**n you Stormcloaks. Skyrim was fine until you came along.')
+        print('lokir: Empire was nice and lazy. If they hadnt been looking for you')
+        print('Lokir: I could’ve stolen that horse and been half way to Hammerfell')
+        print('Lokir: You there. You and me — we shouldnt be here. It’s these Stormcloaks')
+        print('Lokir: the Empire wants.')
+        print('Guard: Be quiet back there you scum')
+        print('the other Guard: We are at the keep, prepare your necks')
+        print('Lokir: wait i shouldnt be here, i have no idea who this stormcloak man is')
+        print('As Lokir runs away from the execution site he gets shot down by a nearby archer')
+        print('do you wish to stay and await execution, or attempt to run away(y/n)')
